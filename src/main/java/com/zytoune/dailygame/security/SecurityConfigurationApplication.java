3f7d767d7f89faec1f110b5e4456bac21c793587
OlivierSTEPHAN <git@github.com>
2024-06-15 21:45:26 +0200

    Ssl que lorsque mode prod, on ajoute le characteristics en autorisé pour le cors

--- src/main/java/com/zytoune/dailygame/security/SecurityConfigurationApplication.java
+++ src/main/java/com/zytoune/dailygame/security/SecurityConfigurationApplication.java
@@ -30,6 +30,7 @@ public class SecurityConfigurationApplication {
                 .authorizeHttpRequests(
                         authorize -> authorize
                                 .requestMatchers(HttpMethod.GET, "/daily-games/screenshots/*").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/daily-games/characteristics/*").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/daily-games/*").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/daily-games/*").permitAll()
                                 .requestMatchers(HttpMethod.GET, "/games").permitAll()
